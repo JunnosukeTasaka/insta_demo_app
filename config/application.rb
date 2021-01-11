bc4417eb5933ca599d5488eaabf1a7986db65e34
Junnosuke Tasaka <junnosuke0120@gmail.com>
2021-01-11 22:27:39 +0900

    add: i18nの日本語ファイル追加

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module InstaDemoApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    # i18n
+    config.i18n.default_locale = :ja
     # rails gの際に、slimを指定
     config.generators do |g|
       g.template_engine :slim
